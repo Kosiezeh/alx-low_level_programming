bbd3f0b7e26fe99e55a2f629bcf1b8dfdc8fbed2
Emmanuel Ezendiokwere <kosiezeh92@gmail.com>
2022-09-22 03:31:16 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,58 +1,17 @@
 #include "main.h"
-
 /**
- * _strlen - function that return the lenght of a string
- * @s: the string
- * Return: length of s
+ *reverse_array - function that reverse content of array
+ *@a: array
+ *@n: number of elements
  */
 
-int _strlen(char *s)
+void reverse_array(int *a, int n)
 {
-	int i;
-
-	i = 0;
-	while (*(s + i))
-		i++;
-
-	return (i);
+int tmp, index;
+for (index = n - 1; index > n / 2; index--)
+{
+tmp = a[n - 1 - index];
+a[n - 1 - index] = a[index];
+a[index] = tmp;
 }
-
-/**
- * _strncpy - function that copies the string pointed by src, included the
- * teminate null byte (\0), to the buffer pointed to by dest
- * @dest: the first parameter
- * @src: second prameter
- * @n: bytes from src that the function will use
- * Return: the pointer to dest
- */
-
-char *_strncpy(char *dest, char *src, int n)
-{
-	int ld, ls, nn;
-
-	ld = _strlen(dest);
-	ls = _strlen(src) - 1;
-
-	if (n <= 0)
-		return (dest);
-	n --;
-
-	if (n > ls)
-		n = ls;
-	while (n >= 0)
-	{
-		*(dest + n) = *(src + n);
-		n--;
-	}
-
-	nn = ld - n;
-	if (nn > 0)
-	{
-		while (nn > 0)
-		{
-			*(dest + ls + nn) = '\0';
-			nn--;
-		}
-	}
-	return (dest);
 }
